32fa2efa9aa0948444d9fc9128402d2359cd1e4d
liyuan <yuali@nvidia.com>
2023-03-16 14:50:53 +0800

    test data validation for hive format
    Signed-off-by: liyuan <yuali@nvidia.com>

--- data_validation/src/spark_rapids_validation_tool/validation_scripts/dataset_validation.py
+++ data_validation/src/spark_rapids_validation_tool/validation_scripts/dataset_validation.py
@@ -26,11 +26,11 @@ def validation(spark, args):
 
     # valid PK(s) only in table1
     result = valid_pk_only_in_one_table(spark, args.format, args.t1, args.t2, args.t1p, args.t2p, args.pk, args.e, args.i, args.f, args.o, args.of)
-    print(f"PK(s) only in {args.t1} :")
+    print(f'|--PK(s) only in {args.t1} :--|')
     print(result.show())
     # valid PK(s) only in table2
     result = valid_pk_only_in_one_table(spark, args.format, args.t2, args.t1, args.t1p, args.t2p, args.pk, args.e, args.i, args.f, args.o, args.of)
-    print(f"PK(s) only in {args.t2} :")
+    print(f'|--PK(s) only in {args.t2} :--|')
     print(result.show())
 
     # valid result table with the same PK but different values for that column(s)
@@ -134,10 +134,12 @@ def valid_pk_only_in_one_table(spark, format, t1, t2, t1p, t2p, pk, e, i, f, o, 
 
         print('------yua-debug-validat-----')
         print(sql)
-        result = spark.sql(sql+"ddd")
+        result = spark.sql(sql)
+        print(result.show())
+        print('------yua-debug-validat22-----')
         return result
 
-    return 0
+    return
 
 def get_cols_diff_with_same_pk(spark, format, table1_name, table2_name, pk, partitions, filter, included_columns, excluded_columns):
     if format in ['parquet', 'orc', 'csv']:
