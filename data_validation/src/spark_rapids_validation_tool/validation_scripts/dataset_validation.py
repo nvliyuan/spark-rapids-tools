e4041fe9729bf8dc0bd70abc29c5bd60135c5315
liyuan <yuali@nvidia.com>
2023-03-16 21:36:20 +0800

    support nested map
    Signed-off-by: liyuan <yuali@nvidia.com>

--- data_validation/src/spark_rapids_validation_tool/validation_scripts/dataset_validation.py
+++ data_validation/src/spark_rapids_validation_tool/validation_scripts/dataset_validation.py
@@ -19,6 +19,7 @@ from pyspark.sql.functions import col   # pylint: disable=import-error
 import time
 from pyspark.sql.types import *
 import fnmatch
+from functools import reduce
 
 def validation(spark, args):
 
@@ -187,7 +188,7 @@ def get_cols_diff_with_same_pk(spark, format, table1_name, table2_name, pk, t1p,
         joined_table = table_DF1.alias("t1").join(table_DF2.alias("t2"), pk_list)
 
         map_cols = []
-        cond = True
+        cond = []
         for c in table_DF1.schema.fields:
             print('-----able_DF1.schema.fields-----')
             print(c.dataType.simpleString())
@@ -201,9 +202,9 @@ def get_cols_diff_with_same_pk(spark, format, table1_name, table2_name, pk, t1p,
         print('-------yuadebug-normal_cols-----')
         print(normal_cols)
         for c in normal_cols:
-            cond = cond | ("t1." + c != "t2." + c)
+            cond.append("t1." + c != "t2." + c)
         for c in map_cols:
-            cond = cond | (map_to_string("t1." + c) != map_to_string("t2." + c))
+            cond.append(map_to_string("t1." + c) != map_to_string("t2." + c))
         print('-------yuadebug-cond-----')
         print(cond)
         select_columns = [col('t1.' + p) for p in pk.split(',')] + [(col('t1.' + c).alias('t1_'+ c), col('t2.' + c).alias('t2_' + c)) for c in
@@ -211,7 +212,7 @@ def get_cols_diff_with_same_pk(spark, format, table1_name, table2_name, pk, t1p,
                                                                c not in excluded_columns_list]
         print('-------yuadebug-select_columns-----')
         print(select_columns)
-        result_table = joined_table.select(select_columns).where(cond)
+        result_table = joined_table.select(select_columns).where(reduce(lambda a, b: a | b,cond))
 
         # if partitions != 'None':
         #     partitions = [p.strip() for p in partitions.split("and")]
