ce14e2fd4bab389690a6f29fb7300e609c9b0b3a
liyuan <yuali@nvidia.com>
2023-03-17 13:38:03 +0800

    test nested type for data validation tool
    Signed-off-by: liyuan <yuali@nvidia.com>

--- data_validation/src/spark_rapids_validation_tool/validation_scripts/dataset_validation.py
+++ data_validation/src/spark_rapids_validation_tool/validation_scripts/dataset_validation.py
@@ -207,9 +207,18 @@ def get_cols_diff_with_same_pk(spark, format, table1_name, table2_name, pk, t1p,
             cond.append(map_to_string(col("t1." + c)) != map_to_string(col("t2." + c)))
         print('-------yuadebug-cond-----')
         print(cond)
-        select_columns = [col('t1.' + p) for p in pk.split(',')] + [(col('t1.' + c).alias('t1_'+ c), col('t2.' + c).alias('t2_' + c)) for c in
-                                                               included_columns_list if
-                                                               c not in excluded_columns_list]
+        # select_columns = [col('t1.' + p) for p in pk.split(',')] + [(col('t1.' + c).alias('t1_'+ c), col('t2.' + c).alias('t2_' + c)) for c in
+        #                                                        included_columns_list if
+        #                                                        c not in excluded_columns_list]
+        select_columns_list = [(col('t1.' + c).alias('t1_' + c), col('t2.' + c).alias('t2_' + c)) for c in
+                               included_columns_list if
+                               c not in excluded_columns_list]
+        ##flatten select_columns_list
+        select_columns_flattened_list = [select_column for sublist in select_columns_list for select_column in sublist]
+        select_columns = [col('t1.' + p) for p in pk.split(',')] + select_columns_flattened_list
+        print('-------yuadebug-select_columns-----')
+        print(select_columns)
+        print('-------yuadebug-joined_table-----')
         print('-------yuadebug-select_columns-----')
         print(select_columns)
         print('-------yuadebug-joined_table-----')
